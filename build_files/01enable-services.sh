5f5d7a6211d749d902dc322231835e2584456d21
sleepyoh <36079594+sleepyoh@users.noreply.github.com>
2025-07-05 16:07:59 +0200

    renaming flathub stuff

--- build_files/01enable-services.sh
+++ build_files/01enable-services.sh
@@ -4,7 +4,7 @@ set -euox pipefail
 	# Enabling systemd-services
     systemctl enable podman.socket
     systemctl enable flatpak-add-flathub-repo.service
-	systemctl enable flatpak-replace-fedora-apps.service
+	systemctl enable flatpak-remove-all.service
 	systemctl enable flatpak-cleanup.timer
 	systemctl enable install-my-flatpaks.service
 	systemctl enable rpm-ostreed-automatic.timer
