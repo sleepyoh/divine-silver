e532262e3fc7195dc025331f2b743e8e8d2faf6c
sleepyoh <36079594+sleepyoh@users.noreply.github.com>
2026-07-07 20:23:37 +0200

    Update 00enarpmfus.sh

--- build_files/00enarpmfus.sh
+++ build_files/00enarpmfus.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/bash
 set -euox pipefail
 
-## libpostproc-free \ This is changed in 44 apparently removed and merged. 
-
 rpm-ostree install \
 		https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 		https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 
-rpm-ostree override remove \
-		ffmpeg-free \
-		libavcodec-free \
-		libavdevice-free \
-		libavfilter-free \
-		libavformat-free \
-		libavutil-free \
-		libswresample-free \
-		libswscale-free \
-		--install=ffmpeg \
-		--install=gstreamer1-plugin-libav \
-		--install=gstreamer1-plugins-bad-free-extras \
-		--install=gstreamer1-plugins-bad-freeworld \
-		--install=gstreamer1-plugins-ugly \
-		--install=gstreamer1-vaapi
+# Swap out the core ffmpeg-free stack for full ffmpeg with all dependencies resolved automatically
+dnf5 swap -y ffmpeg-free ffmpeg --allowerasing
+
+# Install the updated GStreamer multimedia plugins
+dnf5 install -y \
+    gstreamer1-plugin-libav \
+    gstreamer1-plugins-bad-free-extras \
+    gstreamer1-plugins-bad-freeworld \
+    gstreamer1-plugins-ugly
