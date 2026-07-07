d723b4ef883f8d59beefc3808ab3e305364d32c6
sleepyoh <36079594+sleepyoh@users.noreply.github.com>
2026-07-07 23:20:27 +0200

    sa

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,23 +5,25 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 echo "Starting build process..."
 
+# Copy the contents of system_files/ of the git repo to /
+
+echo "Copying system-files"
+cp -avf "/ctx/system_files"/. /
+
 echo "installing rpmrepo+mesa+codecs..."
-bash "$SCRIPT_DIR/00enarpmfus.sh"
+bash "$SCRIPT_DIR/00fusionmedia.sh"
 
 echo "Installing and removing packages..."
 bash "$SCRIPT_DIR/01packages.sh"
 
-#echo "Installing copr stuff..."
-#bash "$SCRIPT_DIR/02cachy-kernel.sh"
-
 echo "enabling services..."
-bash "$SCRIPT_DIR/03enable-services.sh"
+bash "$SCRIPT_DIR/02systemctl.sh"
 
 echo "Cleaning up..."
-bash "$SCRIPT_DIR/04rmetc.sh"
+bash "$SCRIPT_DIR/03osrls.sh"
 
 echo "Cleaning up..."
-bash "$SCRIPT_DIR/05cleanup.sh"
+bash "$SCRIPT_DIR/04cleanup.sh"
 
 echo "Build process completed."
 
